223955b39782064bb4154e9de40c49bf4589aa1e
齐庆 <cnuusw@hotmail.com>
2017-03-12 23:59:33 +0800

    新建控制器修正lawout链接，初步圈定功能范围

---
+++ EasyFast.Web/Areas/Admin/Controllers/ModelController.cs
@@ -0,0 +1,18 @@
+﻿using EasyFast.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyFast.Web.Areas.Admin.Controllers
+{
+    public class ModelController : EasyFastControllerBase
+    {
+        // GET: Admin/Model
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EasyFast.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using EasyFast.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyFast.Web.Areas.Admin.Controllers
+{
+    public class UserController : EasyFastControllerBase
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 管理员管理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AdminList()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 会员管理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult UserList()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 角色管理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult RoleList()
+        {
+            return View();
+        }
+    }
+}
